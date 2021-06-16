a20ef2b77d608faecaa6cc14c967549406558aa7
william <william.cntt@gmail.com>
2021-06-16 02:02:48 -0300

    remove task contollers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Notifier } from "./controllers/NotifierController.js"
 import { Dom } from "./controllers/DomController.js"
-import { Task } from "./controllers/TaskController.js"
 
 const App = {
   async init() {
     await Notifier.request()
     Dom.init()
     console.log('All right')
-    Task.init()
   }
 }
 
