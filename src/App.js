6259a2e9f96f83ed175bbecbcbd63a47dce7b7d9
william <william.cntt@gmail.com>
2021-06-16 02:03:05 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Notifier } from "./controllers/NotifierController.js"
 import { Dom } from "./controllers/DomController.js"
+import { Timer } from "./controllers/TimerController.js"
 
 const App = {
   async init() {
     await Notifier.request()
     Dom.init()
     console.log('All right')
+    Timer.init()
   }
 }
 
